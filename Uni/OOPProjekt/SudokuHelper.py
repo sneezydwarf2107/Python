b92c5a71a440ce475ee30283744da67bcd936cb2
Lukas Schwörer <honolulu2107@gmail.com>
2019-05-22 14:57:23 +0200

    Changed SudokuHelpers to a class

--- Uni/OOPProjekt/SudokuHelper.py
+++ Uni/OOPProjekt/SudokuHelper.py
@@ -1,65 +1,96 @@
-def print_sudoku(sudoku):
-
-    print("                             ")
-    for i in range(len(sudoku[0])):
-        if (i % 3 == 0) and (i != 0):
-            print("         ")
-
-        for j in range(len(sudoku)):
-
-            if j % 3 == 0 and j != 0:
-                print("  ", end="")
-
-            print(str(sudoku[i][j]) + "  ", end="")
-
-            if j == 8:
-                print("")
+class Helper:
 
 
-def check(sudoku, candidate, position):
-    """
-    This function is checking if candidate exists in either the column, row or square it is located
+    def print_sudoku(self, sudoku):
+        """
+        Prints out the sudoku puzzle and visualizes it
 
-    :param sudoku: 2D-list, containing the sudoku
-    :param candidate: integer value, the value that has to be checked
-    :param position: list [Column, Row], containing the position of the empty cell
 
-    :return: TRUE - if candidate is valid, FALSE - if candidate is not valid
-    """
-    #Check Row
-    for i in range(len(sudoku[0])):
 
-        if sudoku[position[0]][i] == candidate and i != position[1]:
-            return False
+        :param sudoku: Sudoku puzzle
+        """
+        print("                             ")
+        for i in range(len(sudoku[0])):
+            if (i % 3 == 0) and (i != 0):
+                print("         ")
 
-    #Check Column
-    for i in range(len(sudoku)):
-        if sudoku[i][position[1]] == candidate and i != position[0]:
-            return False
+            for j in range(len(sudoku)):
 
-    #Check Square
-    # First step, calculated the square the empty cell is located in
-    x_cor = int(position[0]/3)*3
-    y_cor = int(position[1]/3)*3
+                if j % 3 == 0 and j != 0:
+                    print("  ", end="")
 
-    # Second step, iterate over the square
-    for i in range(x_cor, x_cor+3):
-        for j in range(y_cor, y_cor+3):
-            if sudoku[i][j] == candidate and [i,j] != position:
+                print(str(sudoku[i][j]) + "  ", end="")
+
+                if j == 8:
+                    print("")
+
+    def check_row(self, sudoku, candidate, position):
+        """
+        Checks if the candidate already exists in the same row, as specified in position[0]
+
+
+
+        :param sudoku: Sudoku puzzle
+        :param candidate: The candidate that has to be checked
+        :param position: The position that has to be checked
+        :return: True if candidate is valid, False if candidate is not valid
+        """
+        # Check Row
+        for i in range(len(sudoku[0])):
+
+            if sudoku[position[0]][i] == candidate and i != position[1]:
                 return False
 
-    return True
-
-
-def is_empty(list):
-    """
-    This function checks if a cell is empty
-
-    :param list:
-    :return:
-    """
-    if list:
-        return False
-
-    else:
         return True
+
+    def check_column(self, sudoku, candidate, position):
+        """
+        Checks if the candidate already exists in the same column, as specified in position[1]
+
+
+
+        :param sudoku: Sudoku puzzle
+        :param candidate: Candidate that has to be checked
+        :param position: Position that has to be checked
+        :return: True if the candidate is valid, False if the candidate is ot valid
+         """
+
+        for i in range(len(sudoku)):
+            if sudoku[i][position[1]] == candidate and i != position[0]:
+                return False
+
+        return True
+
+    def check_square(self, sudoku, candidate, position):
+        """
+        Checks if the candidate already exists in the same square, as indicated in position
+
+        :param sudoku: Sudoku puzzle
+        :param candidate: Candidate that has to be checked
+        :param position: Position that has to be checked
+        :return: True if candidate is valid, False if candidate is not valid
+        """
+        # First step, calculated the square the empty cell is located in
+        x_cor = int(position[0] / 3) * 3
+        y_cor = int(position[1] / 3) * 3
+
+        # Second step, iterate over the square
+        for i in range(x_cor, x_cor + 3):
+            for j in range(y_cor, y_cor + 3):
+                if sudoku[i][j] == candidate and [i, j] != position:
+                    return False
+
+        return True
+
+    def is_empty(self, list):
+        """
+        This function checks if a cell is empty
+
+        :param list:
+        :return:
+        """
+        if list:
+            return False
+
+        else:
+            return True
--- Uni/OOPProjekt/mainSudoku.py
+++ Uni/OOPProjekt/mainSudoku.py
@@ -1,13 +1,14 @@
 from SudokuHelper import *
-
+import sys
 
 def main_solver(sudoku):
+    #print_sudoku(sudoku)
 
-    print_sudoku(sudoku)
+    #print(sudoku[0])
 
     position = 0
     found = False
-    #Step 1 - Leeres Feld finden
+    # Step 1 - Leeres Feld finden
 
     for i in range(len(sudoku[0])):
         for j in range(len(sudoku)):
@@ -19,18 +20,18 @@ def main_solver(sudoku):
             break
     # Step 3 - Ausgabe wenn Lösung gefunden
     if position == 0:
-
         print("=============================")
         print("===========SOLVED============")
         print("=============================")
 
         print_sudoku(sudoku)
-        exit(0)
 
-    #Step 2 - Versuche Werte 1 - 9 und rufe Programm dann rekursiev auf
+        sys.exit(None)
 
-    for i in range(1,10):
-        if check(sudoku, i, position):
+    # Step 2 - Versuche Werte 1 - 9 und rufe Programm dann rekursiev auf
+
+    for i in range(1, 10):
+        if check_Row(sudoku, i, position) and check_Column(sudoku, i, position) and check_Square(sudoku, i, position):
             sudoku[position[0]][position[1]] = i
             main_solver(sudoku)
             sudoku[position[0]][position[1]] = 0
@@ -38,15 +39,16 @@ def main_solver(sudoku):
 
 
 if __name__ == "__main__":
+    sudoku =[[0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,3,0,8,5],
+[0,0,1,0,2,0,0,0,0],
+[0,0,0,5,0,7,0,0,0],
+[0,0,4,0,0,0,1,0,0],
+[0,9,0,0,0,0,0,0,0],
+[5,0,0,0,0,0,0,7,3],
+[0,0,2,0,1,0,0,0,0],
+[0,0,0,0,4,0,0,0,9]]
 
-    sudoku = [[4, 1, 0, 0, 6, 5, 0, 0, 7],
-              [0, 0, 6, 0, 0, 7, 4, 8, 0],
-              [2, 0, 7, 4, 9, 0, 0, 0, 6],
-              [0, 6, 0, 0, 7, 0, 1, 0, 0],
-              [3, 0, 1, 5, 0, 0, 0, 7, 2],
-              [0, 9, 0, 0, 4, 2, 3, 0, 8],
-              [1, 0, 8, 6, 0, 0, 0, 2, 9],
-              [0, 2, 0, 0, 1, 8, 6, 4, 0],
-              [6, 0, 0, 3, 0, 0, 0, 1, 0]]
+    print_sudoku(sudoku)
 
     main_solver(sudoku)
